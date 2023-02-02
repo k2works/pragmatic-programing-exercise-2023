93a7a14d7cf78f19c996a007b75b9ca02bb3d472
k2works <kakimomokkuri@gmail.com>
2023-02-02 16:49:19 +0900

    test: 繰り返し処理について

---
+++ chapter02/infinity/go.mod
@@ -0,0 +1,3 @@
+module infinity
+
+go 1.19
---
+++ chapter02/infinity/infinity_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestInfinity(t *testing.T) {
+	i := 0
+
+	for {
+		t.Log(i)
+		i++
+
+		if i == 5 {
+			break
+		}
+	}
+}
