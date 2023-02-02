b9b3f2ff5c8ed622e8ac331ae64d68627c8254e9
k2works <kakimomokkuri@gmail.com>
2023-02-02 10:03:54 +0900

    test: インクリメント文とデクリメント文

---
+++ chapter02/inc_dec/go.mod
@@ -0,0 +1,3 @@
+module github.com/k2works/inc_dec
+
+go 1.19
---
+++ chapter02/inc_dec/inc_dec_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func Test_main(t *testing.T) {
+	var inc int = 0
+	inc++
+
+	expected := 1
+	actual := inc
+	if actual != expected {
+		t.Errorf("Incorrect answer. Got: %v, Expected: %v", actual, expected)
+	}
+
+	var dec int = 0
+	dec--
+
+	expected = -1
+	actual = dec
+	if actual != expected {
+		t.Errorf("Incorrect answer. Got: %v, Expected: %v", actual, expected)
+	}
+}
