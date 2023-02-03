55458399d00dcc35f04b97301aa853ad858edb29
k2works <kakimomokkuri@gmail.com>
2023-02-03 09:27:01 +0900

    test: 条件分岐について

--- chapter02/main.go
+++ chapter02/main.go
@@ -19,6 +19,7 @@ import (
 	_ "chapter02/section016/infinity"
 	_ "chapter02/section016/range"
 	_ "chapter02/section016/range_strng"
+	_ "chapter02/section017/falthrough"
 	_ "chapter02/section017/if"
 	_ "chapter02/section017/swithch"
 )
