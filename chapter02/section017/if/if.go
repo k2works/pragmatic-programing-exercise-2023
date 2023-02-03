a958e7d002250a3c26ebff3792a2e37e75c5bcc5
k2works <kakimomokkuri@gmail.com>
2023-02-03 09:15:20 +0900

    test: 条件分岐について

---
+++ chapter02/section017/if/if.go
@@ -0,0 +1,13 @@
+package if_
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 5; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, "は偶数")
+		} else {
+			fmt.Println(i, "は奇数")
+		}
+	}
+}
---
+++ chapter02/section017/if/if_test.go
@@ -0,0 +1,13 @@
+package if_
+
+import "testing"
+
+func Test_main(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		if i%2 == 0 {
+			t.Log(i, "は偶数")
+		} else {
+			t.Log(i, "は奇数")
+		}
+	}
+}
