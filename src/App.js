f0033d8358e89fd5da9a68faaa2874ef95a43e5c
k2works <kakimomokkuri@gmail.com>
2023-03-13 18:09:03 +0900

    test: 関数型プログラミング

--- src/App.js
+++ src/App.js
@@ -8,3 +8,41 @@ export class App {
 export function sum(a, b) {
   return a + b;
 }
+
+export function totalStructured() {
+  let total = 0;
+  const prices = [1, 5, 7];
+
+  for (let n = 0; n < prices.length; n++) {
+    total += prices[n];
+  }
+
+  return total;
+}
+
+export function totalInject() {
+  const prices = [1, 5, 7];
+  const result = prices.reduce((total, n) => total + n, 0);
+  return result;
+}
+
+export function totalReduce() {
+  const prices = [1, 5, 7];
+  return prices.reduce((total, n) => total + n, 0);
+}
+
+export function iteratorNotUsing() {
+  const result = [];
+  for (let i = 1; i <= 9; i++) {
+    if (i % 2 === 0) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+export function iteratorUsingSelect() {
+  return [1, 2, 3, 4, 5, 6, 7, 8, 9].filter(i => i % 2 === 0);
+}
+
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,70 @@
-import { sum } from "./App.js";
+import { sum, totalInject, totalReduce, totalStructured, iteratorNotUsing, iteratorUsingSelect } from "./App.js";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('totalStructured', () => {
+  expect(totalStructured()).toBe(13);
+});
+
+test('totalFunctiohnal', () => {
+  expect(totalInject()).toBe(13);
+  expect(totalReduce()).toBe(13);
+});
+
+test('iteratorNotUsing', () => {
+  expect(iteratorNotUsing()).toEqual([2, 4, 6, 8]);
+});
+
+test('iteratorUsing', () => {
+  expect(iteratorUsingSelect()).toEqual([2, 4, 6, 8]);
+});
+
+describe('イテレータ', () => {
+  test('繰り返しの処理', () => {
+    const result = [1, 2, 3].map(n => n * n);
+    expect(result).toEqual([1, 4, 9]);
+  });
+
+  test('特定の条件を満たす要素だけを配列に入れて返す: filter', () => {
+    const result = [1.1, 2, 3.3, 4].filter(item => Number.isInteger(item));
+    expect(result).toEqual([2, 4]);
+  });
+
+  test('特定の条件を満たさない要素だけを配列に入れて返す: reject', () => {
+    const result = [1.1, 2, 3.3, 4].filter(item => !Number.isInteger(item));
+    expect(result).toEqual([1.1, 3.3]);
+  });
+
+  test('新しい要素の配列を返す: map', () => {
+    const result = ["apple", "orange", "pineapple", "strawberry"].map(item => item.length);
+    expect(result).toEqual([5, 6, 9, 10]);
+  });
+
+  test('配列の中から、条件に一致する要素を取得する: find', () => {
+    const result = ["apple", "orange", "pineapple", "strawberry"].find(item => item.length > 5);
+    expect(result).toBe("orange");
+  });
+
+  test('指定した評価式で並び替えた配列を返す: sort', () => {
+    const result = [1, 2, 10, 20, 3].sort((a, b) => a - b);
+    expect(result).toEqual([1, 2, 3, 10, 20]);
+  });
+
+  test('配列の中から、条件に一致する要素を取得する: filter', () => {
+    const result = ["apple", "orange", "pineapple", "strawberry", "apricot"].filter(item => /^a/.test(item));
+    expect(result).toEqual(["apple", "apricot"]);
+  });
+
+  test('ブロック内の条件式が真である間までの要素を返す: takeWhile', () => {
+    const result = [1, 2, 3, 4, 5, 6, 7, 8, 9].filter(item => item < 6);
+    expect(result).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  test('ブロック内の条件式が真である以降の要素を返す: dropWhile', () => {
+    const result = [1, 2, 3, 4, 5, 6, 7, 8, 9].filter(item => item > 6);
+    expect(result).toEqual([7, 8, 9]);
+  });
+
 });
