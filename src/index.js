e30283b1b87342ef8beb0ee6134206099c12b54e
K2works <k2works@gmail.com>
2023-03-14 17:09:29 +0900

    feat: 画面表示

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import { App } from "./App.js";
+import { ビールの歌, ビールの歌の歌詞 } from "./App.js";
 import { setUp } from "./Dev.js";
 
-const app = new App();
-setUp();
+console.log(ビールの歌(99, 0, ビールの歌の歌詞));
+
+const 歌 = ビールの歌(99, 0, ビールの歌の歌詞);
+
+let body = '';
+
+for(let i = 0; i < 歌.length; i++) {
+  body += `<div>${歌[i].join(" ")}</div>`;
+}
+
+document.getElementById("app").innerHTML = body;
+
