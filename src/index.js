60a093bd54064840d4d35b71e68204ee6396af66
K2works <k2works@gmail.com>
2023-03-14 17:47:48 +0900

    refactor: パイプラインによるループの置き換え

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import { ビールの歌, ビールの歌の歌詞 } from "./App.js";
 
-const 歌 = ビールの歌(99, 0, ビールの歌の歌詞);
-let body = '';
-for (let i = 0; i < 歌.length; i++) {
-  body += `<p>${歌[i].join("<br>")}</p>`;
-}
+const createSongHTML = 歌 => 歌.map(
+  (歌詞, i) => `<p>${歌詞.join("<br>")}</p>`
+).join("");
 
-document.getElementById("titlecontent").innerHTML = body;
+document.getElementById("titlecontent").innerHTML = createSongHTML(ビールの歌(99, 0, ビールの歌の歌詞));
 
