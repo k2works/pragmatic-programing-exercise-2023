fb1a2a70e25642ca29a4432ba50f6db9fab45eee
k2works <kakimomokkuri@gmail.com>
2023-03-14 18:17:58 +0900

    feat: First steps

---
+++ api/http_server.ts
@@ -0,0 +1,18 @@
+import { serve } from "https://deno.land/std@0.179.0/http/server.ts";
+
+const handler = async(_request: Request): Promise<Response> => {
+  const resp = await fetch("https://api.github.com/users/denoland", {
+    headers: {
+      accept: "application/json",
+    },
+  });
+
+  return new Response(resp.body, {
+    status: resp.status,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+};
+
+serve(handler);
