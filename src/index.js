a73c1314ed54a030262e50971a558e5136b2d71e
K2works <k2works@gmail.com>
2023-03-14 17:36:43 +0900

    style: 表示アニメーション

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import { ビールの歌, ビールの歌の歌詞 } from "./App.js";
-import { setUp } from "./Dev.js";
-
-console.log(ビールの歌(99, 0, ビールの歌の歌詞));
 
 const 歌 = ビールの歌(99, 0, ビールの歌の歌詞);
-
 let body = '';
-
-for(let i = 0; i < 歌.length; i++) {
-  body += `<div>${歌[i].join(" ")}</div>`;
+for (let i = 0; i < 歌.length; i++) {
+  body += `<p>${歌[i].join("<br>")}</p>`;
 }
 
-document.getElementById("app").innerHTML = body;
+document.getElementById("titlecontent").innerHTML = body;
 
