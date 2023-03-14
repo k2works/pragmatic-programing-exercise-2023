ed329eca7301bcb079ec535279e99924f8ff9b67
K2works <k2works@gmail.com>
2023-03-14 15:31:28 +0900

    refactor: 変数の抽出

--- src/App.test.js
+++ src/App.test.js
@@ -2,23 +2,24 @@ import { lyricSegment } from "./App.js";
 
 
 describe("lyricSegment", () => {
+  let result;
+  beforeEach(() => {
+    result = lyricSegment();
+  });
+
   test("first line", () => {
-    const result = lyricSegment(99);
     expect(result[0]).toBe("99 bottles of beer on the wall");
   });
 
   test("second line", () => {
-    const result = lyricSegment(99);
     expect(result[1]).toBe("99 bottles of beer");
   });
 
   test("third line", () => {
-    const result = lyricSegment(99);
     expect(result[2]).toBe("Take one down, pass it around");
   });
 
   test("last line", () => {
-    const result = lyricSegment(99);
     expect(result[result.length - 1]).toBe("No more bottles of beer on the wall!");
   });
 });
