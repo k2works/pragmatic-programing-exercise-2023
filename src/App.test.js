dfc46f254f5e4623f266c130cc527cf680fa1b81
k2works <kakimomokkuri@gmail.com>
2023-03-15 13:35:14 +0900

    test: SQLのクエリの書き方

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,38 @@
-import { sum } from "./App.js";
+import { PrismaClient } from "@prisma/client";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
-});
+const prisma = new PrismaClient()
+let students = [];
+beforeAll(async () => {
+  students = await prisma.student.findMany();
+})
+
+describe("SQL入門", () => {
+  describe("SQLのクエリの書き方", () => {
+    test("SELECT id FROM Student", () => {
+      const select = (data) => data.map((i) => i.id);
+
+      const result = select(students);
+
+      console.table(result);
+      expect(result.length).toBe(10);
+    })
+
+    test("SELECT id,test_score FROM Student", () => {
+      const select = (data) => data.map((i) => ({ id: i.id, test_score: i.test_score }));
+
+      const result = select(students);
+
+      console.table(result);
+      expect(result.length).toBe(10);
+    })
+
+    test("SELECT * FROM Student", () => {
+      const select = (data) => data.map((i) => i);
+
+      const result = select(students);
+
+      console.table(result);
+      expect(result.length).toBe(10);
+    });
+  })
+})
