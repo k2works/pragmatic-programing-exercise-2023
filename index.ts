85906601e8607b83a7077ae82153b7ed0d67290e
k2works <kakimomokkuri@gmail.com>
2023-03-23 13:26:39 +0900

    fix: 開発ツールセットアップ

--- index.ts
+++ index.ts
@@ -8,7 +8,23 @@ const contents = `
 ## TODOリスト
 `;
 
+const usecase = `
+@startuml
+left to right direction
+actor "Actor" as ac
+rectangle Application {
+  usecase "UseCase1" as UC1
+  usecase "UseCase2" as UC2
+  usecase "UseCase3" as UC3
+}
+ac --> UC1
+ac --> UC2
+ac --> UC3
+@enduml
+`;
+
 const uml = `
+@startuml
 abstract class AbstractList
 abstract AbstractCollection
 interface List
@@ -28,9 +44,11 @@ enum TimeUnit {
   MINUTES
 }
 annotation SuppressWarnings
+@enduml
 `;
 
 const erd = `
+@startuml
 ' hide the spot
 hide circle
 ' avoid problems with angled crows feet
@@ -55,5 +73,6 @@ entity "Entity03" as e03 {
 }
 e01 ||..o{ e02
 e01 |o..o{ e03
+@enduml
 `;
-dev.render({ contents, uml, erd });
+dev.render({ contents, usecase, uml, erd });
