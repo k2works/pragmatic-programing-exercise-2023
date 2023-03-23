7fdb434596b3e70f084eb208fcd2814a00bdbd7f
k2works <kakimomokkuri@gmail.com>
2023-03-23 13:04:51 +0900

    chore: 開発ツールアップデート

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import { App } from './src/app.js';
 const app = new App();
 
-import { render } from '@k2works/full-stack-lab';
+import render from "@k2works/full-stack-lab";
 const contents = `
 ## 機能名
 ## 仕様
 ## TODOリスト
 `;
 
+const usecase = `
+@startuml
+left to right direction
+actor "Actor" as ac
+rectangle Application {
+  usecase "UseCase1" as UC1
+  usecase "UseCase2" as UC2
+  usecase "UseCase3" as UC3
+}
+ac --> UC1
+ac --> UC2
+ac --> UC3
+@enduml
+`;
+
 const uml = `
+@startuml
 abstract class AbstractList
 abstract AbstractCollection
 interface List
@@ -28,9 +44,11 @@ enum TimeUnit {
   MINUTES
 }
 annotation SuppressWarnings
+@enduml
 `;
 
 const erd = `
+@startuml
 ' hide the spot
 hide circle
 ' avoid problems with angled crows feet
@@ -55,5 +73,6 @@ entity "Entity03" as e03 {
 }
 e01 ||..o{ e02
 e01 |o..o{ e03
+@enduml
 `;
-render({ contents, uml, erd });
+render({ contents, usecase, uml, erd });
