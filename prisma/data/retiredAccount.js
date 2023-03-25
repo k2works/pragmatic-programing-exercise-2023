18fb81dda45b3a11e5281fe68d486f758da388af
k2works <kakimomokkuri@gmail.com>
2023-03-25 16:39:26 +0900

    build: 銀行口座データベースシード

---
+++ prisma/data/retiredAccount.js
@@ -0,0 +1,32 @@
+const retiredAccount = [
+    {
+        number: '0051432',
+        name: 'オダ　シンタロウ',
+        type: '1',
+        balance: 41310,
+        retiredAt: new Date('2021-12-24'),
+    },
+    {
+        number: '0097310',
+        name: 'サイトウ　モモコ',
+        type: '1',
+        balance: 130040,
+        retiredAt: new Date('2020-06-25'),
+    },
+    {
+        number: '0945671',
+        name: 'モリシタ　カズミ',
+        type: '1',
+        balance: 0,
+        retiredAt: new Date('2020-12-18'),
+    },
+    {
+        number: '1017100',
+        name: 'ソネ　タツヤ',
+        type: '1',
+        balance: 0,
+        retiredAt: new Date('2020-11-08'),
+    }
+];
+
+module.exports = retiredAccount;
