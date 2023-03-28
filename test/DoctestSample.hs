0b9dc718af1d67950ae60b8213b0afe6ac6fe998
k2works <kakimomokkuri@gmail.com>
2023-03-28 03:29:59 +0000

    build: テスト、その前に

---
+++ test/DoctestSample.hs
@@ -0,0 +1,19 @@
+module DoctestSample where
+
+-- | 文字列中のスペースの個数
+--
+-- >>> countSpace ""
+-- 0
+-- >>> countSpace "abracadabra"
+-- 0
+-- >>> countSpace "Hello, World!"
+-- 1
+-- >>> countSpace "    "
+-- 4
+--
+-- prop> countSpace s == sum [1 | c <- s, c == ' ']
+--
+countSpace :: String -> Int
+countSpace = length . filter (' ' ==)
+-- スペースだけでなく、うっかりタブ文字もカウントしてしまう
+-- countSpace = length . filter (\x -> ' ' == x || '\t' == x)
