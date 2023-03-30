358998884ec086bf2ae8b3cac23711c2c2af663e
k2works <kakimomokkuri@gmail.com>
2023-03-30 12:46:00 +0900

    test: 第６章 オススメの開発／設計テクニック 「らしからぬ」コード

--- src/app.test.js
+++ src/app.test.js
@@ -4,3 +4,37 @@ test('adds 1 + 2 to equal 3', () => {
   const result = sum(1, 2);
   expect(result).toBe(3);
 });
+
+// ランレングス圧縮
+// rle :: String -> String
+function rle(s) {
+  if (s === '') return '';
+
+  const [h, ...t] = s;
+  return aux(1, h, t, h);
+
+  // aux :: Int -> Char -> String -> Char -> String
+  function aux(runLength, prevChar, str, currentChar) {
+    if (str.length === 0) return prevChar + runLength;
+
+    const [c, ...s] = str;
+    if (c === prevChar) {
+      return aux(runLength + 1, prevChar, s, currentChar);
+    } else {
+      return prevChar + runLength + aux(1, c, s, c);
+    }
+  }
+}
+
+test('rle', () => {
+  expect(rle('')).toBe('');
+  expect(rle('A')).toBe('A1');
+  expect(rle('AA')).toBe('A2');
+  expect(rle('AAA')).toBe('A3');
+  expect(rle('AAAA')).toBe('A4');
+  expect(rle('AB')).toBe('A1B1');
+  expect(rle('ABB')).toBe('A1B2');
+  expect(rle('AABBB')).toBe('A2B3');
+  expect(rle('AABBBB')).toBe('A2B4');
+  expect(rle('AABBBBC')).toBe('A2B4C1');
+});
--- src/app.test.ts
+++ src/app.test.ts
@@ -4,3 +4,36 @@ test('adds 1 + 2 to equal 3', () => {
   const result = sum(1, 2);
   expect(result).toBe(3);
 });
+
+/**
+ * ランレングス圧縮
+ * @param str 入力文字列
+ * @returns 圧縮後の文字列
+ *
+ * @example
+ * rle("") // ""
+ * rle("AAABBCCCAAA") // "A3B2C3A3"
+ */
+function rle(str: string): string {
+  if (str === "") {
+    return "";
+  }
+  const [h, ...t] = str;
+  return aux(1, h, t, h);
+}
+
+function aux(runLength: number, prevChar: string, s: string[], currentChar: string): string {
+  if (s.length === 0) {
+    return prevChar + String(runLength);
+  }
+  const [c, ...rest] = s;
+  if (c === prevChar) {
+    return aux(runLength + 1, prevChar, rest, currentChar);
+  }
+  return prevChar + runLength.toString() + aux(1, c, rest, c);
+}
+
+test("rle", () => {
+  expect(rle("")).toBe("");
+  expect(rle("AAABBCCCAAA")).toBe("A3B2C3A3");
+});
