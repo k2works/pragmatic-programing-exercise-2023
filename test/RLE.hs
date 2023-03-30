9b7e9311ff4485fd1740e0ece3a18ecdec6ca58a
k2works <kakimomokkuri@gmail.com>
2023-03-30 13:17:59 +0900

    test: 第６章 オススメの開発／設計テクニック 「らしい」コード

--- test/RLE.hs
+++ test/RLE.hs
@@ -10,19 +10,10 @@ import Data.List(group)
 -- >>> rle "AAABBCCCAAA"
 -- "A3B2C3A3"
 rle :: String -> String
-rle = fromCharAndRunLength . toCharAndRunLength
-
--- | 文字とその連長の組のリストを出力文字列へ変換する
-fromCharAndRunLength :: [(Char, Int)] -> String
-fromCharAndRunLength = concat . map rl2str
+rle = concat . map rl2str . map toPair . group
 
 rl2str :: (Char, Int) -> String
 rl2str (c, n) = c : show n -- nを文字列にして文字cを先頭に付ける
-
--- | 入力文字列とその連長の組のリストへ変換する
-toCharAndRunLength :: String -> [(Char, Int)]
-toCharAndRunLength [] = []
-toCharAndRunLength xs = map toPair $ group xs
 
 toPair :: [a] -> (a, Int)
 toPair xs = (head xs, length xs)
