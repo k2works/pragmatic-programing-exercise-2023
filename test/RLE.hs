f6d3922301ccd3d7a3cee23beee205388f320abc
k2works <kakimomokkuri@gmail.com>
2023-03-30 13:25:28 +0900

    test: 第６章 オススメの開発／設計テクニック 「らしい」コード

--- test/RLE.hs
+++ test/RLE.hs
@@ -10,10 +10,4 @@ import Data.List(group)
 -- >>> rle "AAABBCCCAAA"
 -- "A3B2C3A3"
 rle :: String -> String
-rle = concatMap (rl2str . toPair) . group
-
-rl2str :: (Char, Int) -> String
-rl2str (c, n) = c : show n -- nを文字列にして文字cを先頭に付ける
-
-toPair :: [a] -> (a, Int)
-toPair xs = (head xs, length xs)
+rle = concatMap (\s -> head s : show(length s)). group
