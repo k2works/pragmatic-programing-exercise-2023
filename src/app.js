9d52c94f752654b52b4aa5660509bdf830429d44
k2works <kakimomokkuri@gmail.com>
2023-04-04 13:05:50 +0900

    style: ビジュアルデザイン

--- src/app.js
+++ src/app.js
@@ -5,8 +5,33 @@ export class App {
 
   render() {
     studentCollection();
+    studentSingle();
   }
 }
+
+const students = [
+  {
+    id: 1,
+    name: "田尻 智裕",
+    class: "3年B組",
+    clubs: ["野球部", "写真部"],
+    relatedStudents: ["山田 太郎", "鈴木 花子"]
+  },
+  {
+    id: 2,
+    name: "山田 太郎",
+    class: "3年A組",
+    clubs: ["野球部"],
+    relatedStudents: ["田尻 智裕"]
+  },
+  {
+    id: 3,
+    name: "鈴木 花子",
+    class: "3年A組",
+    clubs: ["写真部"],
+    relatedStudents: ["田尻 智裕"]
+  }
+]
 
 const rootNavigation = () => {
   const container = document.getElementById("app");
@@ -34,73 +59,31 @@ const rootNavigation = () => {
 }
 
 const studentCollection = () => {
+  const record = students.map((student) => {
+    return `
+      <li class="main-object-item">
+        <div class="main-object-item-content">
+          <div class="main-object-item-name">${student.name}</div>
+          <div class="main-object-item-details">${student.class} ${student.clubs.join(" ")}</div>
+        </div>
+        <div class="main-object-item-actions">
+          <button class="action-button" id="delete" data=${student.id}>削除</button>
+          <button class="action-button" id="edit" data=${student.id}>編集</button>
+        </div>
+      </li>
+    `
+  });
+
   const container = document.getElementById("contents");
   container.innerHTML = `
         <button class="action-button" id="create">新規</button>
         <div class="main-object-container">
           <ul class="main-object-list">
-            <li class="main-object-item">
-              <div class="main-object-item-content">
-                <div class="main-object-item-name">田尻 智裕</div>
-                <div class="main-object-item-details">3年B組 野球部 写真部</div>
-              </div>
-              <div class="main-object-item-actions">
-                <button class="action-button" id="delete">削除</button>
-                <button class="action-button" id="edit">編集</button>
-              </div>
-            </li>
-            <li class="main-object-item">
-              <div class="main-object-item-content">
-                <div class="main-object-item-name">山田 太郎</div>
-                <div class="main-object-item-details">3年A組 野球部</div>
-              </div>
-              <div class="main-object-item-actions">
-                <button class="action-button">削除</button>
-                <button class="action-button">編集</button>
-              </div>
-            </li>
-            <li class="main-object-item">
-              <div class="main-object-item-content">
-                <div class="main-object-item-name">田尻 智裕</div>
-                <div class="main-object-item-details">3年B組 野球部 写真部</div>
-              </div>
-              <div class="main-object-item-actions">
-                <button class="action-button">削除</button>
-                <button class="action-button">編集</button>
-              </div>
-            </li>
-            <li class="main-object-item">
-              <div class="main-object-item-content">
-                <div class="main-object-item-name">山田 太郎</div>
-                <div class="main-object-item-details">3年A組 野球部</div>
-              </div>
-              <div class="main-object-item-actions">
-                <button class="action-button">削除</button>
-                <button class="action-button">編集</button>
-              </div>
-            </li>
-            <li class="main-object-item">
-              <div class="main-object-item-content">
-                <div class="main-object-item-name">田尻 智裕</div>
-                <div class="main-object-item-details">3年B組 野球部 写真部</div>
-              </div>
-              <div class="main-object-item-actions">
-                <button class="action-button">削除</button>
-                <button class="action-button">編集</button>
-              </div>
-            </li>
-            <li class="main-object-item">
-              <div class="main-object-item-content">
-                <div class="main-object-item-name">山田 太郎</div>
-                <div class="main-object-item-details">3年A組 野球部</div>
-              </div>
-              <div class="main-object-item-actions">
-                <button class="action-button">削除</button>
-                <button class="action-button">編集</button>
-              </div>
-            </li>
-          </ul>
-        </div>
+            ` + record.join("") + `
+            ` + record.join("") + `
+            ` + record.join("") + `
+          </div>
+        </div>     
   `;
 
   const createButton = document.getElementById("create");
@@ -117,57 +100,110 @@ const studentCollection = () => {
 
   const editButtons = document.querySelectorAll("#edit");
   editButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      studentSingle();
+    button.addEventListener("click", (evnt) => {
+      const id = evnt.target.getAttribute("data");
+      const student = students.find((student) => {
+        return student.id === Number(id);
+      });
+      studentSingle(student);
     });
   });
 }
 
-const studentSingle = () => {
+const studentSingle = (student) => {
+  const header = student ? `
+    <div class="single-view-header-left">
+      <h1 class="single-view-title">${student.name}</h1>
+      <p class="single-view-subtitle">${student.class}</p>
+    </div>
+    <div class="single-view-header-right">
+      <button class="single-view-action-button" id="edit">編集</button>
+      <button class="single-view-action-button" id="delete">削除</button>
+    </div>
+  ` : `
+    <div class="single-view-header-left">
+      <h1 class="single-view-title"></h1>
+      <p class="single-view-subtitle"></p>
+    </div>
+  `;
+
+  const main = student ? `
+    <div class="single-view-body-left">
+      <ul class="single-view-property-list">
+        <li class="single-view-property-item">
+          <span class="single-view-property-label">名前:</span>
+          <span class="single-view-property-value">${student.name}</span>
+        </li>
+        <li class="single-view-property-item">
+          <span class="single-view-property-label">組:</span>
+          <span class="single-view-property-value">${student.class}</span>
+        </li>
+      </ul>
+    </div>
+    ` : `
+    <div class="single-view-body-left">
+      <ul class="single-view-property-list">
+        <li class="single-view-property-item">
+          <span class="single-view-property-label">名前:</span>
+          <span class="single-view-property-value"></span>
+        </li>
+        <li class="single-view-property-item">
+          <span class="single-view-property-label">組:</span>
+          <span class="single-view-property-value"></span>
+        </li>
+      </ul>
+    </div>
+  `;
+
+  const clubs = student ? student.clubs.map((club) => {
+    return `
+      <li class="single-view-related-item">${club}</li>
+    `
+  }) : [];
+
+  const relatedStudents = student ? student.relatedStudents.map((relatedStudent) => {
+    return `
+      <li class="single-view-related-item">${relatedStudent}</li>
+    `
+  }) : [];
+
   const container = document.getElementById("contents");
   container.innerHTML = `
         <div class=".main-single-view-object-container">
           <div class="single-view-container">
             <div class="single-view-header">
-              <div class="single-view-header-left">
-                <h1 class="single-view-title">田尻 智裕</h1>
-                <p class="single-view-subtitle">3年B組</p>
-              </div>
-              <div class="single-view-header-right">
-                <button class="single-view-action-button">編集</button>
-                <button class="single-view-action-button">削除</button>
-              </div>
+            ` + header + `
             </div>
             <div class="single-view-body">
-              <div class="single-view-body-left">
-                <ul class="single-view-property-list">
-                  <li class="single-view-property-item">
-                    <span class="single-view-property-label">名前:</span>
-                    <span class="single-view-property-value">田尻 智裕</span>
-                  </li>
-                  <li class="single-view-property-item">
-                    <span class="single-view-property-label">組:</span>
-                    <span class="single-view-property-value">3年B組</span>
-                  </li>
-                </ul>
-              </div>
+            ` + main + `
               <div class="single-view-body-right">
                 <h2 class="single-view-related-title">部</h2>
                 <ul class="single-view-related-list">
-                  <li class="single-view-related-item">野球部</li>
-                  <li class="single-view-related-item">写真部</li>
+                ` + clubs.join("") + `
                 </ul>
               </div>
               <div class="single-view-body-right">
                 <h2 class="single-view-related-title">関連する生徒</h2>
                 <ul class="single-view-related-list">
-                  <li class="single-view-related-item">田尻 智裕</li>
-                  <li class="single-view-related-item">山田 太郎</li>
-                  <li class="single-view-related-item">鈴木 花子</li>
+                ` + relatedStudents.join("") + `
                 </ul>
               </div>
             </div>
           </div>
         </div>     
   `;
+
+  const editButton = document.getElementById("edit");
+  if (editButton) {
+    editButton.addEventListener("click", () => {
+      studentSingle(student);
+    });
+  }
+
+  const deleteButton = document.getElementById("delete");
+  if (deleteButton) {
+    deleteButton.addEventListener("click", () => {
+      alert("削除しますか？")
+    });
+  }
 }
