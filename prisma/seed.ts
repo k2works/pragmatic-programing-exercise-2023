3035c4c2e3b3181541fd03978518470d0a1dc3c7
k2works <kakimomokuri@gmail.com>
2023-04-19 18:46:16 +0900

    test: 部門マスタ

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,31 +1,36 @@
-import { PrismaClient, employee } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient, dept_mst, employee } from "@prisma/client";
+const prisma = new PrismaClient();
+import { departments } from "./data/department";
+
 async function main() {
-  await prisma.employee.deleteMany({})
-  const employee: employee = await prisma.employee.create(
-    {
-      data: {
-      emp_code: 'EMP001',
-      emp_name: 'John',
-      emp_kana: 'ジョン',
-      login_password: 'password',
-      tel: '090-1234-5678',
-      fax: '03-1234-5678',
-      dept_code: '000001',
-      start_date: new Date('2021-01-01'),
-      occu_code: '',
-      approval_code: '',
-      },
-    }
-  );
-  console.log({ employee})
+  await prisma.dept_mst.deleteMany({});
+  await prisma.dept_mst.createMany({
+    data: departments,
+  });
+
+  await prisma.employee.deleteMany({});
+  const employee: employee = await prisma.employee.create({
+    data: {
+      emp_code: "EMP001",
+      emp_name: "John",
+      emp_kana: "ジョン",
+      login_password: "password",
+      tel: "090-1234-5678",
+      fax: "03-1234-5678",
+      dept_code: "000001",
+      start_date: new Date("2021-01-01"),
+      occu_code: "",
+      approval_code: "",
+    },
+  });
+  console.log({ employee });
 }
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
