0a8c118e66451991eca81929a5339f894da9bb27
k2works <kakimomokkuri@gmail.com>
2023-04-21 17:56:25 +0900

    test: 取引先マスタの設計

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, customers_mst, consumer, supplier_mst } from "@prisma/client";
+
 const prisma = new PrismaClient();
 import { departments } from "./data/department";
 import { employees } from "./data/employee";
@@ -14,11 +15,11 @@ async function main() {
       where: {
         dept_code_start_date: {
           dept_code: dept.dept_code,
-          start_date: dept.start_date,
-        },
+          start_date: dept.start_date
+        }
       },
       create: dept,
-      update: dept,
+      update: dept
     });
   }
 
@@ -26,10 +27,10 @@ async function main() {
   for (const emp of employees) {
     await prisma.employee.upsert({
       where: {
-        emp_code: emp.emp_code,
+        emp_code: emp.emp_code
       },
       create: emp,
-      update: emp,
+      update: emp
     });
   }
 
@@ -37,10 +38,10 @@ async function main() {
   for (const cate of productCategories) {
     await prisma.product_category.upsert({
       where: {
-        category_code: cate.category_code,
+        category_code: cate.category_code
       },
       create: cate,
-      update: cate,
+      update: cate
     });
   }
 
@@ -48,10 +49,10 @@ async function main() {
   for (const prod of products) {
     await prisma.products.upsert({
       where: {
-        prod_code: prod.prod_code,
+        prod_code: prod.prod_code
       },
       create: prod,
-      update: prod,
+      update: prod
     });
   }
 
@@ -59,10 +60,10 @@ async function main() {
   for (const comp of companys) {
     await prisma.companys_mst.upsert({
       where: {
-        comp_code: comp.comp_code,
+        comp_code: comp.comp_code
       },
       create: comp,
-      update: comp,
+      update: comp
     });
   }
 
@@ -72,14 +73,108 @@ async function main() {
       where: {
         prod_code_comp_code: {
           prod_code: price.prod_code,
-          comp_code: price.comp_code,
-        },
+          comp_code: price.comp_code
+        }
       },
       create: price,
-      update: price,
+      update: price
     });
   }
+  await prisma.customers_mst.deleteMany()
+  await prisma.customers_mst.createMany({
+    data: [
+      {
+        cust_code: "SUP001",
+        cust_sub_no: 1,
+        cust_type: 0,
+        ar_code: "AR0001",
+        ar_sub_no: 1,
+        payer_code: "CUST0001",
+        payer_sub_no: 1,
+        cust_name: "Customer 1",
+        cust_kana: "カスタマー1",
+        emp_code: "EMP0001",
+        cust_user_name: "Customer 1",
+        cust_user_dep_name: "Customer 1",
+        cust_zip_code: "1234567",
+        cust_state: "0001",
+        cust_address1: "Chiyoda-ku",
+        cust_address2: "1-1-1",
+        cust_tel: "0123456789",
+        cust_fax: "0123456789",
+        cust_email: "hoge@hoge.com",
+        cust_ar_flag: 0,
+        cust_close_date1: 1,
+        cust_pay_months1: 1,
+        cust_pay_dates1: 1,
+        cust_pay_method1: 1,
+        cust_close_date2: 1,
+        cust_pay_months2: 1,
+        cust_pay_dates2: 1,
+        cust_pay_method2: 1,
+        create_date: new Date(),
+        creator: "admin",
+        update_date: new Date(),
+        updater: "admin"
+      }
+    ]
+  });
+
+  await prisma.consumer.deleteMany()
+  await prisma.consumer.createMany({
+    data: [
+      {
+        consumer_code: "CON001",
+        last_name: "Consumer",
+        first_name: "1",
+        last_name_kana: "コンシューマー",
+        first_name_kana: "1",
+        login_id: "consumer1",
+        email: "hoge@hoge.com",
+        pwd: "123456",
+        birth_date: new Date("2000-01-01"),
+        sex: 1,
+        login_datetime: new Date(),
+        rest_point: 100,
+        withdrawal_date: new Date(),
+        create_date: new Date(),
+        creator: "admin",
+        update_date: new Date(),
+        updater: "admin"
+      },
+    ],
+  });
+
+  await prisma.supplier_mst.deleteMany()
+  await prisma.supplier_mst.createMany({
+    data: [
+      {
+        sup_code: "SUP001",
+        sup_sub_no: 1,
+        sup_name: "Supplier 1",
+        sup_kana: "スプライヤー1",
+        sup_emp_name: "Supplier 1",
+        sup_dep_name: "Supplier 1",
+        sup_zip_code: "1234567",
+        sup_state: "0001",
+        sup_address1: "Chiyoda-ku",
+        sup_address2: "1-1-1",
+        sup_tel: "0123456789",
+        sup_fax: "0123456789",
+        sup_email: "hoge@hoge.com",
+        sup_close_date: 1,
+        sup_pay_months: 1,
+        sup_pay_dates: 1,
+        pay_method_type: 1,
+        create_date: new Date(),
+        creator: "admin",
+        update_date: new Date(),
+        updater: "admin"
+      },
+      ]
+  });
 }
+
 main()
   .then(async () => {
     await prisma.$disconnect();
