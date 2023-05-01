7784f91071a5ecd3f3c266c23d5c4e9751e011da
k2works <kakimomokuri@gmail.com>
2023-05-01 09:56:11 +0900

    test: 文字列探索

--- src/api/chap07_test.py
+++ src/api/chap07_test.py
@@ -37,6 +37,46 @@ def bf_match(txt: str, pat: str) -> int:
 # ## KMP法
 # %%
 
+
+class TestKmp(unittest.TestCase):
+    def test_kmp_match(self):
+        self.assertEqual(kmp_match("ABC", "ABC"), 0)
+        self.assertEqual(kmp_match("ABC", "ABD"), -1)
+        self.assertEqual(kmp_match("ABC", "ABABC"), -1)
+
+
+def kmp_match(txt: str, pat: str) -> int:
+    """KMP法による文字列探索"""
+    pr = 1  # txtをなぞるカーソル
+    pt = 0  # patをなぞるカーソル
+    skip = [0] * (len(pat) + 1)  # スキップテーブル
+
+    # スキップテーブルの作成
+    skip[pr] = 0
+    while pr != len(pat):
+        if pat[pr] == pat[pt]:
+            pr += 1
+            pt += 1
+            skip[pr] = pt
+        elif pt == 0:
+            pr += 1
+            skip[pr] = pt
+        else:
+            pt = skip[pt]
+
+    # 探索
+    pt = pr = 0
+    while pt != len(txt) and pr != len(pat):
+        if txt[pt] == pat[pr]:
+            pt += 1
+            pr += 1
+        elif pr == 0:
+            pt += 1
+        else:
+            pr = skip[pr]
+
+    return pt - pr if pr == len(pat) else -1
+
 # %% [markdown]
 # ## Boyer-Moore法
 # %%
