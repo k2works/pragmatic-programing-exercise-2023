b1bd14b4d8f8ab47754f589d136fe1f310e6724f
k2works <kakimomokkuri@gmail.com>
2023-05-12 15:03:08 +0900

    test: 商品マスタ

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,6 +1,15 @@
 import { config } from "dotenv";
 config({ path: '.env.test' })
-import { PrismaClient, Department, Employee } from "@prisma/client";
+import {
+  PrismaClient,
+  Department,
+  Employee,
+  ProductCategory,
+  Product,
+  PriceByCustomer,
+  AlternateProduct,
+} from "@prisma/client";
+import { kMaxLength } from "buffer";
 const prisma = new PrismaClient();
 
 
@@ -12,7 +21,7 @@ const departments: Department[] = [
     name: "新規部署",
     layer: 1,
     psth: "D0001",
-    bottomType: 1,
+    lowestType: 1,
     slitYn: 0,
     createDate: new Date("2021-01-01"),
     creator: "admin",
@@ -37,6 +46,69 @@ const employees: Employee[] = [
     creator: "admin",
     updateDate: new Date("2021-01-01"),
     updater: "admin",
+  }
+];
+
+const productCategories: ProductCategory[] = [
+  {
+    categoryCode: "00101001",
+    name: "牛肉",
+    layer: 0,
+    path: "00100000~00101000~00101001",
+    lowestType: 1,
+    createDate: new Date("2021-01-01"),
+    creator: "user",
+    updateDate: new Date("2021-01-01"),
+    updater: "user",
+  },
+]
+
+const products: Product[] = [
+  {
+    prodCode: "1010100X",
+    fullname: "商品名",
+    name: "商品名",
+    kana: "ショウヒンメイ",
+    prodType: "1",
+    serialNo: "1234567890",
+    unitprice: 1000,
+    poPrice: 900,
+    primeCost: 500,
+    taxType: 1,
+    categoryCode: "00101001",
+    wideUseType: 1,
+    stockManageType: 1,
+    stockReserveType: 1,
+    supCode: "001",
+    supSubNo: 1,
+    createDate: new Date("2021-01-01"),
+    creator: "user",
+    updateDate: new Date("2021-01-01"),
+    updater: "user",
+  }
+];
+
+const alternateProducts: AlternateProduct[] = [
+  {
+    prodCode: '1010100X',
+    altProdCode: 'P0000001',
+    priority: 1,
+    createDate: new Date(),
+    creator: 'admin',
+    updateDate: new Date(),
+    updater: 'admin',
+  }
+];
+
+const priceByCustomers: PriceByCustomer[] = [
+  {
+    prodCode: '1010100X',
+    compCode: 'C0000001',
+    unitprice: 1000,
+    createDate: new Date(),
+    creator: 'admin',
+    updateDate: new Date(),
+    updater: 'admin',
   }
 ];
 
@@ -126,6 +198,75 @@ describe("Part 1 業務システムの概要とマスタ設計", () => {
         const result = await prisma.employee.findMany();
         expect(result).toEqual(expected);
       });
+    });
+
+    describe("商品マスタ", () => {
+      beforeAll(async () => {
+        await prisma.alternateProduct.deleteMany();
+        await prisma.priceByCustomer.deleteMany();
+        await prisma.product.deleteMany();
+        await prisma.productCategory.deleteMany();
+      });
+
+      test("商品を登録できる", async () => {
+        const expected: Product[] = products.map((c) => {
+          return {
+            ...c,
+            alternateProducts: alternateProducts,
+            pricebycustomers: priceByCustomers,
+          };
+        });
+        await prisma.$transaction(async (prisma) => {
+          await prisma.productCategory.createMany({ data: productCategories });
+          await prisma.product.createMany({ data: products });
+          await prisma.alternateProduct.createMany({ data: alternateProducts });
+          await prisma.priceByCustomer.createMany({ data: priceByCustomers });
+        });
+
+        const result = await prisma.product.findUnique({
+          where: { prodCode: products[0].prodCode },
+          include: {
+            alternateProducts: true,
+            pricebycustomers: true,
+          }
+        });
+        expect(result).toEqual(expected[0]);
+      });
+
+      test("商品を更新できる", async () => {
+        const expected: Product[] = products.map((c) => {
+          return { ...c, name: "更新商品" };
+        });
+        await prisma.product.update({
+          where: { prodCode: products[0].prodCode },
+          data: expected[0]
+        });
+
+        const result = await prisma.product.findUnique({
+          where: { prodCode: products[0].prodCode },
+        });
+        expect(result).toEqual(expected[0]);
+      });
+
+      test("商品を削除できる", async () => {
+        const expected: Product[] = [];
+
+        await prisma.$transaction(async (prisma) => {
+          await prisma.alternateProduct.deleteMany({
+            where: { prodCode: products[0].prodCode }
+          });
+          await prisma.priceByCustomer.deleteMany({
+            where: { prodCode: products[0].prodCode }
+          });
+          await prisma.product.delete({
+            where: { prodCode: products[0].prodCode }
+          });
+        });
+
+        const result = await prisma.product.findMany();
+        expect(result).toEqual(expected);
+      });
+
     });
   });
 });
