04a345121e83c589cd88490941c7c5ffded63dc6
k2works <kakimomokuri@gmail.com>
2023-05-16 09:34:10 +0900

    test: 循環・住連結リスト

--- src/api/chap08_test.py
+++ src/api/chap08_test.py
@@ -566,6 +566,289 @@ class ArrayLinkedListIterator:
 # %%
 
 
+class TestDoubleLinkedLinst(unittest.TestCase):
+    def test_循環重連結リストを生成(self):
+        dllist = DoubleLinkedList()
+        self.assertEqual(len(dllist), 0)
+        self.assertEqual(dllist.is_empty(), True)
+
+    def test_先頭に挿入(self):
+        dllist = DoubleLinkedList()
+        dllist.add_first(1)
+        self.assertEqual(len(dllist), 1)
+        self.assertEqual(dllist.is_empty(), False)
+        dllist.add_first(2)
+        self.assertEqual(len(dllist), 2)
+        self.assertEqual(dllist.is_empty(), False)
+
+    def test_末尾に挿入(self):
+        dllist = DoubleLinkedList()
+        dllist.add_last(1)
+        self.assertEqual(len(dllist), 1)
+        self.assertEqual(dllist.is_empty(), False)
+        dllist.add_last(2)
+        self.assertEqual(len(dllist), 2)
+        self.assertEqual(dllist.is_empty(), False)
+
+    def test_着目の直後に挿入(self):
+        dllist = DoubleLinkedList()
+        dllist.add_first(1)
+        dllist.add_first(2)
+        dllist.add_first(3)
+        dllist.next()
+        dllist.add(4)
+        self.assertEqual(len(dllist), 4)
+        self.assertEqual(dllist.is_empty(), False)
+
+    def test_先頭を削除(self):
+        dllist = DoubleLinkedList()
+        dllist.add_first(1)
+        dllist.add_first(2)
+        dllist.add_first(3)
+        dllist.remove_first()
+        self.assertEqual(len(dllist), 2)
+        self.assertEqual(dllist.is_empty(), False)
+
+    def test_末尾を削除(self):
+        dllist = DoubleLinkedList()
+        dllist.add_first(1)
+        dllist.add_first(2)
+        dllist.add_first(3)
+        dllist.remove_last()
+        self.assertEqual(len(dllist), 2)
+        self.assertEqual(dllist.is_empty(), False)
+
+    def test_着目を進める(self):
+        dllist = DoubleLinkedList()
+        dllist.add_first(1)
+        dllist.add_first(2)
+        dllist.add_first(3)
+        dllist.next()
+        dllist.next()
+        self.assertEqual(dllist.get_current_data(), 1)
+
+    def test_着目を戻す(self):
+        dllist = DoubleLinkedList()
+        dllist.add_first(1)
+        dllist.add_first(2)
+        dllist.add_first(3)
+        dllist.next()
+        dllist.next()
+        dllist.prev()
+        self.assertEqual(dllist.get_current_data(), 2)
+
+    def test_着目を削除(self):
+        dllist = DoubleLinkedList()
+        dllist.add_first(1)
+        dllist.add_first(2)
+        dllist.add_first(3)
+        dllist.next()
+        dllist.remove_current_node()
+        self.assertEqual(len(dllist), 2)
+        self.assertEqual(dllist.is_empty(), False)
+
+    def test_全削除(self):
+        dllist = DoubleLinkedList()
+        dllist.add_first(1)
+        dllist.add_first(2)
+        dllist.add_first(3)
+        dllist.clear()
+        self.assertEqual(len(dllist), 0)
+        self.assertEqual(dllist.is_empty(), True)
+
+    def test_探索(self):
+        dllist = DoubleLinkedList()
+        dllist.add_first(1)
+        dllist.add_first(2)
+        dllist.add_first(3)
+        self.assertEqual(dllist.search(2), 1)
+        self.assertEqual(dllist.search(3), 0)
+        self.assertEqual(dllist.search(4), -1)
+
+
+class Node2:
+    """循環・重連結リスト用のノードクラス"""
+
+    def __init__(self, data: Any = None, prev: Node2 = None, next: Node2 = None) -> None:
+        """初期化"""
+        self.data = data  # データ
+        self.prev = prev or self  # 先行ポインタ
+        self.next = next or self  # 後続ポインタ
+
+
+class DoubleLinkedList:
+    """循環・重連結リスト"""
+
+    def __init__(self) -> None:
+        """初期化"""
+        self.head = self.current = Node2()  # ダミーノードを生成
+        self.no = 0
+
+    def __len__(self) -> int:
+        """線形リスト上のノード数を返却する"""
+        return self.no
+
+    def get_current_data(self) -> Any:
+        """着目ノードのデータを返す"""
+        return self.current.data
+
+    def is_empty(self) -> bool:
+        """リストは空か"""
+        return self.head.next is self.head
+
+    def search(self, data: Any) -> Any:
+        """dataと等価なノードを探索"""
+        cnt = 0
+        ptr = self.head.next      # 現在走査中のノード
+        while ptr is not self.head:
+            if data == ptr.data:
+                self.current = ptr
+                return cnt        # 探索成功
+            cnt += 1
+            ptr = ptr.next        # 後続ノードに着目
+        return -1                 # 探索失敗
+
+    def __contains__(self, data: Any) -> bool:
+        """リストにdataと等価なノードが含まれるか"""
+        return self.search(data) >= 0
+
+    def print_current_node(self) -> None:
+        """着目ノードを表示"""
+        if self.is_empty():
+            print('着目ノードはありません。')
+        else:
+            print(self.current.data)
+
+    def print(self) -> None:
+        """全ノードを表示"""
+        ptr = self.head.next      # 先頭ノード
+        while ptr is not self.head:
+            print(ptr.data)
+            ptr = ptr.next
+
+    def print_reverse(self) -> None:
+        """全ノードを逆順に表示"""
+        ptr = self.head.prev      # 末尾ノード
+        while ptr is not self.head:
+            print(ptr.data)
+            ptr = ptr.prev
+
+    def next(self) -> bool:
+        """着目ノードを一つ後方に進める"""
+        if self.is_empty() or self.current.next is self.head:
+            return False         # 進めることはできなかった
+        self.current = self.current.next
+        return True
+
+    def prev(self) -> bool:
+        """着目ノードを一つ前方に進める"""
+        if self.is_empty() or self.current.prev is self.head:
+            return False         # 進めることはできなかった
+        self.current = self.current.prev
+        return True
+
+    def add(self, data: Any) -> None:
+        """先頭にノードを挿入"""
+        node = Node2(data, self.head, self.head.next)
+        self.head.next.prev = node
+        self.head.next = node
+        self.current = node
+        self.no += 1
+
+    def add_first(self, data: Any) -> None:
+        """先頭にノードを挿入"""
+        self.current = self.head    # ダミーノードheadの直後に挿入
+        self.add(data)
+
+    def add_last(self, data: Any) -> None:
+        """末尾にノードを挿入"""
+        self.current = self.head.prev
+        self.add(data)
+
+    def remove_current_node(self) -> None:
+        """着目ノードを削除"""
+        if not self.is_empty():
+            self.current.prev.next = self.current.next
+            self.current.next.prev = self.current.prev
+            self.current = self.current.prev
+            self.no -= 1
+            if self.current is self.head:
+                self.current = self.head.next
+
+    def remove(self, p: None) -> None:
+        """ノードpを削除"""
+        ptr = self.head.next
+
+        while ptr is not self.head:
+            if ptr is p:          # pを発見
+                self.current = p
+                self.remove_current_node()
+                break
+            ptr = ptr.next
+
+    def remove_first(self) -> None:
+        """先頭ノードを削除"""
+        self.current = self.head.next
+        self.remove_current_node()
+
+    def remove_last(self) -> None:
+        """末尾ノードを削除"""
+        self.current = self.head.prev
+        self.remove_current_node()
+
+    def clear(self) -> None:
+        """全ノードを削除"""
+        while not self.is_empty():  # 空になるまで
+            self.remove_first()     # 先頭ノードを削除
+        self.no = 0
+
+    def __iter__(self) -> DoubleLinkedListIterator:
+        """イテレーターを返却"""
+        return DoubleLinkedListIterator(self.head)
+
+    def __reversed__(self) -> DoubleLinkedListReverseIterator:
+        """逆順イテレーターを返却"""
+        return DoubleLinkedListReverseIterator(self.head)
+
+
+class DoubleLinkedListIterator:
+    """DoubleLinkedListのイテレーター用クラス"""
+
+    def __init__(self, head: Node):
+        self.head = head
+        self.current = head.next
+
+    def __iter__(self) -> DoubleLinkedListIterator:
+        return self
+
+    def __next__(self) -> Any:
+        if self.current is self.head:
+            raise StopIteration
+        else:
+            data = self.current.data
+            self.current = self.current.next
+            return data
+
+
+class DoubleLinkedListReverseIterator:
+    """DoubleLinkedListの降順イテレータ用クラス"""
+
+    def __init__(self, head: Node2):
+        self.head = head
+        self.current = head.prev
+
+    def __iter__(self) -> DoubleLinkedListReverseIterator:
+        return self
+
+    def __next__(self) -> Any:
+        if self.current is self.head:
+            raise StopIteration
+        else:
+            data = self.current.data
+            self.current = self.current.data
+            return data
+
+
 unittest.main(argv=[''], verbosity=2, exit=False)
 doctest.testmod(verbose=True)
 # %%
