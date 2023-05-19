fb6e81eb4d9bb91b60d1f7477e34641bd18f3633
Katuyuki Kakigi <kakimomokuri@gmail.com>
2023-05-19 14:18:23 +0900

    test: 回帰1:映画の興行収入の予測

---
+++ src/api/chap6_test.py
@@ -0,0 +1,28 @@
+# %% [markdown]
+# # 回帰1:映画の興行収入の予測
+
+# %%
+import unittest
+import doctest
+import os
+path = os.path.dirname(os.path.abspath(__file__))
+
+# %% [markdown]
+# ## 映画の興行収入を予測する
+
+# %% [markdown]
+# ## データの前処理
+
+# %% [markdown]
+# ## モデルの作成と学習
+
+# %% [markdown]
+# ## モデルの評価
+
+# %% [markdown]
+# ## 回帰式による影響度の分析
+
+
+# %%
+doctest.testmod(verbose=True)
+unittest.main(argv=[''], verbosity=2, exit=False)
