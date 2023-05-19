9d82b4e1aa4589f03569147a0efb0223b7d0891c
Katuyuki Kakigi <kakimomokuri@gmail.com>
2023-05-19 15:44:08 +0900

    test: 分類2:客船沈没事故での生存予測

---
+++ src/api/chap7_test.py
@@ -0,0 +1,28 @@
+# %% [markdown]
+# # 分類2:客船沈没事故での生存予測
+
+# %%
+import unittest
+import doctest
+import os
+path = os.path.dirname(os.path.abspath(__file__))
+
+# %% [markdown]
+# ## 客船沈没事故から生き残れるかを予測
+
+# %% [markdown]
+# ## データの前処理
+
+# %% [markdown]
+# ## モデルの作成と学習
+
+# %% [markdown]
+# ## モデルの評価
+
+# %% [markdown]
+# ## 決定木における特徴量の考察
+
+
+# %%
+doctest.testmod(verbose=True)
+unittest.main(argv=[''], verbosity=2, exit=False)
