50dff2a455622c7afbbec89d46e06d32f2b2167a
Katuyuki Kakigi <kakimomokuri@gmail.com>
2023-05-27 14:30:12 +0900

    docs: 練習問題:金融機関のキャンペーン分析

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const app = new App();
 import render from "@k2works/full-stack-lab";
 
 const contents = `
-## 機能名
+## [スッキリわかるPythonによる機械学習入門](https://sukkiri.jp/books/sukkiri_ml)
 
-[スッキリわかるPythonによる機械学習入門](https://sukkiri.jp/books/sukkiri_ml)
-
-## 仕様
+## 第１部 ようこそ機械学習の世界へ
 
 - [第4章 機械学習の体験](./notebooks/chap4.html)
 - [第5章 分類1:アヤメの判別](./notebooks/chap5.html)
@@ -17,6 +15,7 @@ const contents = `
 - [第7章 分類2:客船沈没事故での生存確率](./notebooks/chap7.html)
 - [第8章 回帰2:住宅の平均価格の予測](./notebooks/chap8.html)
 - [第9章 練習問題:の金融機関のキャンペーン分析](./notebooks/chap9.html)
+- [第9章 練習問題:の金融機関のキャンペーン分析解答](./notebooks/chap09.html)
 
 `;
 
