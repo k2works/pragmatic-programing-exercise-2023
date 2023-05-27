3f948080b2824954cba49d1b4d2ff18044d044dd
k2works <kakimomokuri@gmail.com>
2023-05-27 13:19:03 +0900

    docs: 基本的なアルゴリズム

--- index.js
+++ index.js
@@ -5,23 +5,14 @@ const app = new App();
 import render from "@k2works/full-stack-lab";
 
 const contents = `
-## 機能名
-## 仕様
-## TODOリスト
+## 新・明解Pythonで学ぶアルゴリズムとデータ構造
+
+### 第1章 [基本的なアルゴリズム](./notebooks/chap01.html)
+
 `;
 
 const usecase = `
 @startuml
-left to right direction
-actor "Actor" as ac
-rectangle Application {
-  usecase "UseCase1" as UC1
-  usecase "UseCase2" as UC2
-  usecase "UseCase3" as UC3
-}
-ac --> UC1
-ac --> UC2
-ac --> UC3
 @enduml
 `;
 
@@ -139,56 +130,13 @@ const uiInteraction = `
 
 const uml = `
 @startuml
-abstract class AbstractList
-abstract AbstractCollection
-interface List
-interface Collection
-List <|-- AbstractList
-Collection <|-- AbstractCollection
-Collection <|- List
-AbstractCollection <|- AbstractList
-AbstractList <|-- ArrayList
-class ArrayList {
-  Object[] elementData
-  size()
-}
-enum TimeUnit {
-  DAYS
-  HOURS
-  MINUTES
-}
-annotation SuppressWarnings
 @enduml
 `;
 
 const erd = `
 @startuml
-' hide the spot
-hide circle
-' avoid problems with angled crows feet
-skinparam linetype ortho
-entity "Entity01" as e01 {
-  *e1_id : number <<generated>>
-  --
-  *name : text
-  description : text
-}
-entity "Entity02" as e02 {
-  *e2_id : number <<generated>>
-  --
-  *e1_id : number <<FK>>
-  other_details : text
-}
-entity "Entity03" as e03 {
-  *e3_id : number <<generated>>
-  --
-  e1_id : number <<FK>>
-  other_details : text
-}
-e01 ||..o{ e02
-e01 |o..o{ e03
 @enduml
 `;
 
-const mode = "APP"; // "UI" or "API"
+const mode = "API"; // "UI" or "API"
 render({ contents, ui, uiModel, uiInteraction, usecase, uml, erd, mode });
