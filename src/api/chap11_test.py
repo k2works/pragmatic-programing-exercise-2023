c33952843042c8810303a4975c3cec587333d56c
Katuyuki Kakigi <kakimomokuri@gmail.com>
2023-05-27 16:20:11 +0900

    docs: さまざまな教師あり学習:回帰

---
+++ src/api/chap11_test.py
@@ -0,0 +1,21 @@
+# %% [markdown]
+# # さまざまな教師あり学習：回帰
+# %%
+import unittest
+import doctest
+import os
+path = os.path.dirname(os.path.abspath(__file__))
+
+# %% [markdown]
+# ## リッジ回帰
+
+# %% [markdown]
+# ## ラッソ回帰
+
+# %% [markdown]
+# ## 回帰木
+
+# %%
+doctest.testmod(verbose=True)
+unittest.main(argv=[''], verbosity=2, exit=False)
+
