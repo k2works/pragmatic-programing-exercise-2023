74f194aa44a1a31fdeac0991e0d7ee2b214d4a21
Katuyuki Kakigi <kakimomokuri@gmail.com>
2023-05-30 15:56:10 +0900

    test: 分類1:アヤメの判別

---
+++ src/api/main_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestHelloWorld(unittest.TestCase):
+
+    def test_hello_world(self):
+        self.assertEqual("Hello World", "Hello World")
+
+
+if __name__ == '__main__':
+    unittest.main()
