9946380da4fca7baf48c3e52fe6e3ebc5199b922
Katuyuki Kakigi <kakimomokuri@gmail.com>
2023-05-30 17:14:23 +0900

    test: 分類1:アヤメの判別

--- src/api/chap09_test.py
+++ src/api/chap09_test.py
@@ -7,14 +7,56 @@ import doctest
 import os
 path = os.path.dirname(os.path.abspath(__file__))
 
+import pandas as pd
+
+class CSVRepository:
+    def __init__(self) -> None:
+        pass
+
+    def get_data(self):
+        return pd.read_csv(path + '/data/iris.csv')
+
+class SQLRepository:
+    def __init__(self) -> None:
+        pass
+
+    def get_data(self):
+        from sqlalchemy import create_engine
+        host = 'localhost'
+        port = '5432'
+        db = 'test'
+        username = 'root'
+        password = 'root'
+
+        engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{db}')
+        return pd.read_sql_table('iris', engine)
+
+repo = CSVRepository()
+
 # %% [markdown]
 # ## データの内容
+# | 列名 | 内容 |
+# | --- | --- |
+# | sepal_length | がく片の長さ |
+# | sepal_width | がく片の幅 |
+# | petal_length | 花弁の長さ |
+# | petal_width | 花弁の幅 |
+# | species | 種類 |
+#
+
+# %%
+df = repo.get_data()
+df.head(3)
+
 
 # %% [markdown]
 # ## 問題背景
+# Iris(アヤメ)のデータは、ある特徴を持つ花がどの種類のアヤメに分類されるかを示すデータです。花の個体情報としてデータの内容の列名が示す4つの特徴量が与えられており、それらの特徴量から花の種類を分類することができます。このデータを用いて、花の特徴量からアヤメの種類を分類するモデルを作成します。
+# 花の種類は、setosa(ヒオウギアヤメ), Iris-versicolor(ブルーフラッグ), Iris-virginica(バージニアアイリス)の3種類があります。
 
 # %% [markdown]
 # ## データ分析の方法検討
+# - 特徴量を「がく片の長さと幅、花びらの長さと幅」として、アヤメの種類を判別する。
 
 # %% [markdown]
 # 分析の実施
