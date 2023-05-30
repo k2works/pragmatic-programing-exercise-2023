0083958bf029a47f33ae0fa6399cd3c2a1550a3c
Katuyuki Kakigi <kakimomokuri@gmail.com>
2023-05-30 15:40:28 +0900

    feat: 分類1:アヤメの判別

--- prisma/csvReader.ts
+++ prisma/csvReader.ts
@@ -1,23 +1,28 @@
-import { User } from "@prisma/client";
+import { Iris } from "@prisma/client";
 import fs from "fs";
 import path from "path";
 
 const encodeing = "utf-8";
 const filePath = (fileName: string) => path.join(`${__dirname}/data`, fileName);
-export const users: User[] = fs
-  .readFileSync(filePath("user.csv"), encodeing)
+export const irises: Iris[] = fs
+  .readFileSync(filePath("iris.csv"), encodeing)
   .trim()
   .split("\n")
   .slice(1)
   .map((line) => {
     const [
-      id,
-      name,
-      email
+      sepal_length,
+      sepal_width,
+      petal_length,
+      petal_width,
+      species,
     ] = line.split(",");
     return {
-      id: parseInt(id),
-      name,
-      email,
+      id: 0,
+      sepal_length: sepal_length ? parseFloat(sepal_length) : null,
+      sepal_width: sepal_width ? parseFloat(sepal_width) : null,
+      petal_length: petal_length ? parseFloat(petal_length) : null,
+      petal_width: petal_width ? parseFloat(petal_width) : null,
+      species,
     };
   });
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-import { users } from "./csvReader";
+import { irises } from "./csvReader";
 
 async function main() {
-  console.table(users)
-  for (const user of users) {
-    await prisma.user.upsert({
-      where: { id: user.id },
+  for (const iris of irises) {
+    console.log(iris)
+    await prisma.iris.upsert({
+      where: { id: iris.id },
       update: {},
       create: {
-        id: user.id,
-        name: user.name,
-        email: user.email,
+        sepal_length: iris.sepal_length,
+        sepal_width: iris.sepal_width,
+        petal_length: iris.petal_length,
+        petal_width: iris.petal_width,
+        species: iris.species,
       },
     });
   }
