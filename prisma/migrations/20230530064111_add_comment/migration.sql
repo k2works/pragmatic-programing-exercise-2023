3267893935595027d2c94081e19701101d414ce1
Katuyuki Kakigi <kakimomokuri@gmail.com>
2023-05-30 15:46:27 +0900

    feat: 分類1:アヤメの判別

---
+++ prisma/migrations/20230530064111_add_comment/migration.sql
@@ -0,0 +1,6 @@
+COMMENT ON TABLE "public"."Iris" IS 'アヤメのデータ';
+COMMENT ON COLUMN "public"."Iris"."sepal_length" IS 'がく片長さ';
+COMMENT ON COLUMN "public"."Iris"."sepal_width" IS 'がく片幅';
+COMMENT ON COLUMN "public"."Iris"."petal_length" IS '花弁長さ';
+COMMENT ON COLUMN "public"."Iris"."petal_width" IS '花弁幅';
+COMMENT ON COLUMN "public"."Iris"."species" IS '種類';
