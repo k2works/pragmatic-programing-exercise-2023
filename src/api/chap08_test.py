abf0b02777d23c3fdd51ac23a5d716cfee800be9
Katuyuki Kakigi <kakimomokuri@gmail.com>
2023-06-02 12:50:27 +0900

    test: 回帰2:住宅の平均価格の予測

--- src/api/chap08_test.py
+++ src/api/chap08_test.py
@@ -5,19 +5,112 @@
 import unittest
 import doctest
 import os
-path = os.path.dirname(os.path.abspath(__file__))
 
+from domain import SQLRepository, convert_categoricals, CategoricalData, DataVisualization
+
+path = os.path.dirname(os.path.abspath(__file__))
+repo = SQLRepository(table='Boston')
+#repo = CSVRepository(file=path + '/data/Boston.csv')
 # %% [markdown]
 # ## データの内容
+# | 列名 | 内容 |
+# | --- | --- |
+# | CRIME | その地域の犯罪発生率(high,low,very_low)|
+# | ZN | 25,000平方フィート以上の住居区画の占める割合 |
+# | INDUS | 小売業以外の商業が占める面積の割合 |
+# | CHAS | チャールズ川の付近かどうかによるダミー変数(1:川の周辺, 0:それ以外) |
+# | NOX | 窒素酸化物の濃度 |
+# | RM | 住居の平均部屋数 |
+# | AGE | 1940年より前に建てられた物件の割合 |
+# | DIS | ボストン市内の5つの雇用施設からの距離 |
+# | RAD | 環状高速道路へのアクセスしやすさ |
+# | TAX | $10,000ドルあたりの不動産税率の総計 |
+# | PTRATIO | 町ごとの教員1人当たりの児童生徒数 |
+# | B | 町ごとの黒人(Bk)の比率を次の式で表したもの。1000(Bk - 0.63)^2 |
+# | LSTAT | 人口における低所得者の割合 |
+# | PRICE | 住宅価格の平均価格 |
+
+# %%
+df = repo.get_data()
+df.head(3)
 
 # %% [markdown]
 # ## 問題背景
 
 # %% [markdown]
 # ## データ分析の方法検討
+# ボストン市内の特定の地域の住宅価格を予測する回帰式を作成し、どのような地域だと価格が高くなりやすいかを考察する。
 
 # %% [markdown]
 # 分析の実施
+
+# %% [markdown]
+# ### データの概要
+
+# %%
+df.info()
+
+# %% [markdown]
+# ### データの統計量
+
+# %%
+df.describe()
+
+# %% [markdown]
+# ### データの特徴量の相関確認
+
+# %%
+categorical_cols = ['PRICE']
+df_conv = convert_categoricals(df, categorical_cols)
+df_conv.corr()
+
+# %% [markdown]
+# ### データの数値変数確認
+
+# %%
+df.select_dtypes(include='number').columns
+
+# %% [markdown]
+# ### データのカテゴリ変数確認
+
+# %%
+df.select_dtypes(include='object').columns
+
+# %% [markdown]
+# ### CRIMEカテゴリ
+
+# %%
+crime = CategoricalData(df, 'CRIME')
+
+# %%
+crime.show()
+
+# %%
+crime.plot()
+
+## %% [markdown]
+# ### 種類カテゴリの数値変換
+
+# %%
+categorical_cols = ['CRIME']
+df_conv = convert_categoricals(df, categorical_cols)
+conv_species = CategoricalData(df_conv, 'CRIME')
+conv_species.plot()
+
+# %% [markdown]
+# ### ダミー変数化
+
+# %%
+df_dummy = crime.dummy()
+df_dummy
+
+# %% [markdown]
+# ### データの可視化
+
+# %%
+dv = DataVisualization(df)
+dv.df_all('PRICE')
+
 
 # %% [markdown]
 # ## データの前処理
