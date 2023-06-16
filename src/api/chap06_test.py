e2b3360550fe71a6c384023c82be1df86f191327
k2works <kakimomokuri@gmail.com>
2023-06-16 10:04:43 +0900

    refactor: ソート

--- src/api/chap06_test.py
+++ src/api/chap06_test.py
@@ -17,6 +17,9 @@ from chap04_test import Stack
 # %% [markdown]
 # ## 単純交換ソート（バブルソート）
 
+# %% [markdown]
+# ### 単純交換ソート（バブルソート）
+
 # %%  [markdown]
 # #### List6-1 単純交換ソート
 # %
@@ -72,14 +75,88 @@ def bubble_sort_verbose(a: MutableSequence) -> None:
         print(f'比較は{ccnt}回でした。')
         print(f'比較は{scnt}回でした。')
 
+# %% [markdown]
+# #### List6-3 単純交換ソート（第2版：交換回数による打ち切り）
+# %%
+
+
+class TestBubleSort2(unittest.TestCase):
+    def test_buble_sort2(self):
+        a = [6, 4, 3, 7, 1, 9, 8]
+        bubble_sort2(a)
+        self.assertEqual(a, [1, 3, 4, 6, 7, 8, 9])
+
+
+def bubble_sort2(a: MutableSequence) -> None:
+    """単純交換ソート（第2版：交換回数による打ち切り）"""
+    n = len(a)
+    for i in range(n - 1):
+        exchng = 0  # パスにおける交換回数
+        for j in range(n - 1, i, -1):
+            if a[j - 1] > a[j]:
+                a[j - 1], a[j] = a[j], a[j - 1]
+                exchng += 1
+        if exchng == 0:
+            break
+
+# %% [markdown]
+# #### List6-4 単純交換ソート（第3版：捜査範囲を限定）
+# %%
+
+
+class TestBubleSort3(unittest.TestCase):
+    def test_buble_sort3(self):
+        a = [6, 4, 3, 7, 1, 9, 8]
+        bubble_sort3(a)
+        self.assertEqual(a, [1, 3, 4, 6, 7, 8, 9])
+
+
+def bubble_sort3(a: MutableSequence) -> None:
+    """単純交換ソート（第3版：捜査範囲の限定）"""
+    n = len(a)
+    k = 0
+    while k < n - 1:
+        last = n - 1
+        for j in range(n - 1, k, -1):
+            if a[j - 1] > a[j]:
+                a[j - 1], a[j] = a[j], a[j - 1]
+                last = j
+        k = last
+
+
+# %% [markdown]
+# #### List6-5 シェーカーソート（双方向バブルソート）
+# %%
+class TestShakerSort(unittest.TestCase):
+    def test_shaker_sort(self):
+        a = [6, 4, 3, 7, 1, 9, 8]
+        shaker_sort(a)
+        self.assertEqual(a, [1, 3, 4, 6, 7, 8, 9])
+
+
+def shaker_sort(a: MutableSequence) -> None:
+    """シェーカーソート（双方向バブルソート）"""
+    left = 0
+    right = len(a) - 1
+    last = right
+    while left < right:
+        for j in range(right, left, -1):
+            if a[j - 1] > a[j]:
+                a[j - 1], a[j] = a[j], a[j - 1]
+                last = j
+            left = last
+
+        for j in range(left, right):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                last = j
+            right = last
+
 
 # %%
 unittest.main(argv=[''], verbosity=2, exit=False)
 doctest.testmod(verbose=True)
 
-
-# %% [markdown]
-# ### 単純交換ソート（バブルソート）
 
 # %% [markdown]
 # ### シューカーソート（双方向バブルソート）
@@ -162,110 +239,6 @@ doctest.testmod(verbose=True)
 # %% [markdown]
 # ### 度数ソート
 
-
-class TestBubleSort(unittest.TestCase):
-    def test_buble_sort(self):
-        a = [6, 4, 3, 7, 1, 9, 8]
-        bubble_sort(a)
-        self.assertEqual(a, [1, 3, 4, 6, 7, 8, 9])
-
-    def test_buble_sort_verbose(self):
-        a = [6, 4, 3, 7, 1, 9, 8]
-        bubble_sort_verbose(a)
-        self.assertEqual(a, [1, 3, 4, 6, 7, 8, 9])
-
-    def test_buble_sort2(self):
-        a = [6, 4, 3, 7, 1, 9, 8]
-        bubble_sort2(a)
-        self.assertEqual(a, [1, 3, 4, 6, 7, 8, 9])
-
-    def test_buble_sort3(self):
-        a = [6, 4, 3, 7, 1, 9, 8]
-        bubble_sort3(a)
-        self.assertEqual(a, [1, 3, 4, 6, 7, 8, 9])
-
-    def test_shaker_sort(self):
-        a = [6, 4, 3, 7, 1, 9, 8]
-        shaker_sort(a)
-        self.assertEqual(a, [1, 3, 4, 6, 7, 8, 9])
-
-
-def bubble_sort(a: MutableSequence) -> None:
-    """単純交換ソート(バブルソート)"""
-    n = len(a)
-    for i in range(n - 1):
-        for j in range(n - 1, i, -1):
-            if a[j - 1] > a[j]:
-                a[j - 1], a[j] = a[j], a[j - 1]
-
-
-def bubble_sort_verbose(a: MutableSequence) -> None:
-    """単純交換ソート（ソート過程を表示)"""
-    ccnt = 0  # 比較回数
-    scnt = 0  # 交換回数
-    n = len(a)
-    for i in range(n - 1):
-        print(f'パス{i + 1}')
-        for j in range(n - 1, i, -1):
-            for m in range(0, n - 1):
-                print(f'{a[m]:2}' + ('  ' if m != j - 1 else
-                                     ' +' if a[j - 1] > a[j] else ' -'),
-                      end='')
-                print(f'{a[n - 1]:2}')
-                ccnt += 1
-                if a[j - 1] > a[j]:
-                    scnt += 1
-                    a[j - 1], a[j] = a[j], a[j - 1]
-            for m in range(0, n-1):
-                print(f'{a[m]:2}', end='  ')
-            print(f'{a[n - 1]:2}')
-        print(f'比較は{ccnt}回でした。')
-        print(f'比較は{scnt}回でした。')
-
-
-def bubble_sort2(a: MutableSequence) -> None:
-    """単純交換ソート（第2版：交換回数による打ち切り）"""
-    n = len(a)
-    for i in range(n - 1):
-        exchng = 0  # パスにおける交換回数
-        for j in range(n - 1, i, -1):
-            if a[j - 1] > a[j]:
-                a[j - 1], a[j] = a[j], a[j - 1]
-                exchng += 1
-        if exchng == 0:
-            break
-
-
-def bubble_sort3(a: MutableSequence) -> None:
-    """単純交換ソート（第3版：捜査範囲の限定）"""
-    n = len(a)
-    k = 0
-    while k < n - 1:
-        last = n - 1
-        for j in range(n - 1, k, -1):
-            if a[j - 1] > a[j]:
-                a[j - 1], a[j] = a[j], a[j - 1]
-                last = j
-        k = last
-
-
-def shaker_sort(a: MutableSequence) -> None:
-    """シェーカーソート（双方向バブルソート）"""
-    left = 0
-    right = len(a) - 1
-    last = right
-    while left < right:
-        for j in range(right, left, -1):
-            if a[j - 1] > a[j]:
-                a[j - 1], a[j] = a[j], a[j - 1]
-                last = j
-            left = last
-
-        for j in range(left, right):
-            if a[j] > a[j + 1]:
-                a[j], a[j + 1] = a[j + 1], a[j]
-                last = j
-            right = last
 
 # %% [markdown]
 # ## 単純選択ソート
